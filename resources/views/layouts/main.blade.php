da7e7e766f1029e7670106cedc037b417c40b7ae
kirill-02 <136984088+kirill-02@users.noreply.github.com>
2025-05-23 18:56:24 +0400

    q

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -21,7 +21,7 @@
         background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%28255, 255, 255, 1%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e");
     }
 </style>
-<body>
+<body style="background: #e8edd5; ">
 <div class="container">
     <nav class="navbar navbar-expand-lg text-white background-green">
         <div class="container-fluid ">
